128737359f1440ce1c37c8db90957271c5d8b086
Vishalrajpurohit04 <vishalrajpurohit0004@gmail.com>
2026-03-02 23:12:10 +0530

    Revise comparison logs for null and undefined
    Updated comparison examples with null and undefined in JavaScript.

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,16 +8,16 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
 
 // the reason is that equallity == and comparisons > >= < <= work differently.comparison convert null into number and treat its as 0. thats why null >= 0 is true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);//undefined
+console.log(undefined > 0);//undefined
+console.log(undefined < 0);//undefined
 
-// === 
+// === => its check both type and value for a data type
 
-console.log("2" === 2);
+console.log("2" === 2);//false
